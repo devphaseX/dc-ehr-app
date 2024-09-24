c7ad554a11e437d896a16a8645698a1f8e01c2dc
Ayomide Lawal <ayomidelawal700@gmail.com>
2024-09-24 06:25:21 +0100

    remove collection tab on profile

--- src/app/(pages)/(app)/(routes)/(protected)/resources/[id]/@info/profile.tsx
+++ src/app/(pages)/(app)/(routes)/(protected)/resources/[id]/@info/profile.tsx
@@ -61,6 +61,8 @@ export const AuthorProfile = ({ user }: { user: User }) => {
                 </p>
                 <p className="text-sm text-neutral-500">Publication</p>
               </div>
+
+              {/*
               <div className="self-stretch  relative">
                 <Separator
                   orientation="vertical"
@@ -71,6 +73,7 @@ export const AuthorProfile = ({ user }: { user: User }) => {
                 <p className="text-neutral-700 font-semibold text-base">9</p>
                 <p className="text-sm text-neutral-500">Collections</p>
               </div>
+                */}
             </div>
           </div>
           <Link
